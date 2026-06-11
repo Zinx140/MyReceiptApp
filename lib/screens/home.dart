0b128e54048f681fa7224038dca6eda5b457b1b1
Zinx140 <frostocean4@gmail.com>
2026-06-11 21:24:20 +0700

    init

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -107,7 +107,7 @@ class _HomeState extends State<Home> {
       setState(() {
         selectedImage = File(returnedImage.path);
       });
-````
+
       Navigator.push(
         context,
         MaterialPageRoute(
